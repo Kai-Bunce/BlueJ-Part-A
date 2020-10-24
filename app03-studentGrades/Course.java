dbdcbbeef6d421e46e6fc85cec1632e956f146ec
Kai-Bunce <71985097+Kai-Bunce@users.noreply.github.com>
2020-10-24 16:39:23 +0100

    Added ability to add a module

--- app03-studentGrades/Course.java
+++ app03-studentGrades/Course.java
@@ -8,7 +8,7 @@
 public class Course
 {
     // instance variables 
-    private String codeNo;
+    private String courseID;
     private String title;
     
     private Module module1;
@@ -19,23 +19,42 @@ public class Course
     /**
      * Constructor for objects of class Course
      */
-    public Course(String codeNo,String title)
+    public Course(String id,String title)
     {
         // initialise instance variables
-        this.codeNo = codeNo;
+        this.courseID = id;
         this.title = title;
-        
-        module1 = new Module("Programming Concepts", "CO452");
     }
 
     
     public void addModule(Module module, int moduleNo)
     {
+       if(moduleNo == 1)
+       {
+           module1 = module;
+       }
+       else if (moduleNo == 2)
+       {
+           module2 = module;
+       }
+       else if (moduleNo == 3)
+       {
+           module3 = module;
+       }
+       else if (moduleNo == 4)
+       {
+           module4 = module;
+       }
+       else 
+       {
+           System.out.println("Invalid module number");
+       }
+        
        
     }  
-    
-    /**
-     * Prints out the details of a course
-     */
-  
+    public char calculateGrade()
+    {
+        
+    }
+
 }
